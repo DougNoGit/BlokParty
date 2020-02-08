7a077cccdecbadbf54993318bdc0c1aea5a25e4b
Elleis <souzaelleis@gmail.com>
2020-02-08 14:30:29 -0800

    Revert "Merge branch 'depthbuffering' into master"
    This reverts commit 6c9a3b7dc4e16333ed5f5a7a1ac828b5b9d768ea, reversing
    changes made to a4cf360b4eb2ed08ae0c6e1fd01de731f781b47e.

--- src/main.cc
+++ src/main.cc
@@ -15,9 +15,9 @@
 using SimpleVertexBuffer = VertexAttributeBuffer<SimpleVertex>;
 using SimpleVertexInput = VertexInputTemplate<SimpleVertex>;
 
-struct Transforms {    
-    alignas(16) glm::mat4 Model;
+struct Transforms {
     alignas(16) glm::mat4 View;    
+    alignas(16) glm::mat4 Model;
     alignas(16) glm::mat4 Projection;
 };
 
@@ -153,9 +153,9 @@ void Application::render(){
     angle+=0.01;
     // Set the value of our uniform variable
     mTransformUniforms->pushUniformData({
-        glm::translate(glm::vec3(.1*cos(time), .1*sin(time), -5)) * glm::rotate(angle, glm::vec3(0,1,0)),
         glm::mat4(1),
-        getPerspective(frameDimensions, 120, 0.1, 150)
+        glm::translate(glm::vec3(.1*cos(time), .1*sin(time), -5)) * glm::rotate(angle, glm::vec3(0,1,0)),
+        getPerspective(frameDimensions, 90, 0.1, 150),
     });
     mAnimationUniforms->pushUniformData({time});
 
@@ -165,7 +165,7 @@ void Application::render(){
 
 void Application::initGeometry(){
 
-    ModelContainer mc = ModelContainer("../assets/suzanne.gltf");
+    ModelContainer mc = ModelContainer("../assets/cube.gltf");
 
 
     // Create a new vertex buffer on the GPU using the given geometry 
@@ -180,8 +180,7 @@ void Application::initGeometry(){
     const static SimpleVertexInput vtxInput( /*binding = */ 0U,
         /*vertex attribute descriptions = */ {
             {0, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(SimpleVertex, pos)},
-            {1, 0, VK_FORMAT_R32G32B32A32_SFLOAT, offsetof(SimpleVertex, color)},
-            {2, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(SimpleVertex, normal)}
+            {1, 0, VK_FORMAT_R32G32B32A32_SFLOAT, offsetof(SimpleVertex, color)}
         }
     );
     // Send this description to the GPU so that it knows how to interpret our vertex buffer 
